78e57c3ea798bf8f5bf5c68db8e5afac3c6d946c
daemanchi <naurvv@kaist.ac.kr>
2020-12-20 15:58:49 +0900

    #Config ts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>My TS App</h1>
+    <p><input type="text" v-model="inputText" /></p>
+    <p>Count: {{ count }}</p>
+    <p><button @click="reset()">Reset</button></p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+interface State {
+  inputText: string;
 }
+
+export default defineComponent({
+  name: 'App',
+  data: (): State => {
+    return {
+      inputText: ''
+    }
+  },
+  computed: {
+    count (): number {
+      return this.inputText.length
+    }
+  },
+  methods: {
+    reset () {
+      this.inputText = ''
+    }
+  },
+});
 </script>
 
 <style>
